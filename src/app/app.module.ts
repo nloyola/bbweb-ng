82849ee3936c55a7d743690703c731328c1c5d78
Nelson Loyola <nloyola@gmail.com>
2018-07-18 09:27:48 -0600

    Fixes for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
-
-const routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent }
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,8 +13,8 @@ const routes = [
     BrowserModule,
     SharedModule,
     HomeModule,
+    AppRoutingModule,
     NgbModule.forRoot(),
-    RouterModule.forRoot(routes)
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    HomeRoutingModule
   ],
   declarations: [HomeComponent],
   exports: [HomeComponent]
