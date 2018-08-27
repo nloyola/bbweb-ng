d38408024ca42d228d4ba0f54a5acf27c9c85809
Nelson Loyola <nloyola@gmail.com>
2018-08-27 08:41:43 +0200

    More editor config

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((fill-column . 110)
+         (eval . (progn
+                   (setq dumb-jump-default-project (projectile-project-root)
+                         projectile-test-suffix-function (lambda (project-type) "" ".spec")))))))
