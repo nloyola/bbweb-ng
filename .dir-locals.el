0b5b8219e3f5803796941f989c8f02a81350db2c
Nelson Loyola <nloyola@gmail.com>
2019-04-27 11:32:23 +0200

    Fix for loading Emacs configuration

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,7 @@
-((nil . ((fill-column . 96)
+((nil . ((fill-column . 110)
          (eval . (progn
-                   (use-package nl-angular :load-path "~/.emacs.d")
+                   (use-package nl-angular
+                     :load-path "~/.emacs.d"
+                     :init
+                     (require 'tide))
                    (setq dumb-jump-default-project (projectile-project-root)))))))
