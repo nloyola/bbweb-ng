9c1e039e6d34d588378e9161e2e30822a48cdf52
Nelson Loyola <nloyola@gmail.com>
2019-10-07 16:18:01 +0200

    Fixed Emacs warning

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
          (eval . (progn
                    (use-package nl-angular
                      :after typescript-mode
-                     :load-path "~/.emacs.d"
+                     :load-path "~/.emacs.d/lisp"
                      :init
                      (require 'tide))
                    (setq dumb-jump-default-project (projectile-project-root)))))))
