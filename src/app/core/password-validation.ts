d7cd7d7a504814b10e72ffbd481286678f340e60
Nelson Loyola <nloyola@gmail.com>
2021-03-02 17:16:37 +0100

    wip: working on logging in

--- src/app/core/password-validation.ts
+++ src/app/core/password-validation.ts
@@ -1,4 +1,5 @@
 import { AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
+
 export class PasswordValidation {
   static matchingPasswords(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors => {
